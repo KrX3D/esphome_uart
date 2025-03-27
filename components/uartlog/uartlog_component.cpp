83eb4a32461a275e2d8932ab3f4c220dea426352
KrX3D <KrX3D@users.noreply.github.com>
2025-03-27 19:52:03 +0100

    ds

--- components/uartlog/uartlog_component.cpp
+++ components/uartlog/uartlog_component.cpp
@@ -26,8 +26,7 @@ void UartLogComponent::setup() {
   this->uart_->begin(this->baud_rate);
 #endif
   // Send an initial message on UART and print to logger.
-  this->uart_->println("####################################\n####################################"
-  );
+  this->uart_->println("####################################\n####################################\n########## UART Log started ########\n####################################\n####################################");
   ESP_LOGI(TAG, "UART Log started on TX pin %d with baud %d", this->tx_pin, this->baud_rate);
 
 #ifdef USE_LOGGER
