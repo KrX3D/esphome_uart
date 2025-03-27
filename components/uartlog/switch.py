06f0075035d5625cac7ef4600252a1c853390ffa
KrX3D <KrX3D@users.noreply.github.com>
2025-03-27 19:38:03 +0100

    ds

--- components/uartlog/switch.py
+++ components/uartlog/switch.py
@@ -1,20 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
-from . import uartlog_ns, UartLogComponent
 
-UartLogSwitch = uartlog_ns.class_("UartLogSwitch", switch.Switch, cg.Component)
+uartlog_ns = cg.esphome_ns.namespace("uartlog")
+UartLogSwitch = empty_switcuartlog_nsh_ns.class_("UartLogComponent", switch.Switch, cg.Component)
 
-UARTLOG_SWITCH_SCHEMA = switch.switch_schema(UartLogSwitch).extend({
-    cv.Required("parent"): cv.use_id(UartLogComponent),  # Attach to UartLogComponent
-})
+CONFIG_SCHEMA = switch.switch_schema(UartLogSwitch).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
-    parent = await cg.get_variable(config["parent"])
     var = await switch.new_switch(config)
-
-    # Attach switch to parent
-    cg.add(var.set_parent(parent))
-    
     await cg.register_component(var, config)
