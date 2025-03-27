4bb8e07a16b5ea1806af858fafce8d9211a21aeb
KrX3D <KrX3D@users.noreply.github.com>
2025-03-28 00:10:02 +0100

    s

--- components/uartlog/__init__.py
+++ components/uartlog/__init__.py
@@ -11,13 +11,15 @@ CONF_TX_PIN = "tx_pin"
 CONF_STRIP_COLORS = "strip_colors"
 CONF_MIN_LEVEL = "min_level"
 
-# Define namespace and classes.
+# Create a namespace for our component.
 uartlog_ns = cg.esphome_ns.namespace("uartlog")
-# Note: UartLogComponent now inherits from both cg.Component and switch.Switch.
+
+# Define the main UART log component.
+# Note: We inherit from both cg.Component and switch.Switch so that our component acts as a switch.
 UartLogComponent = uartlog_ns.class_("UartLogComponent", cg.Component, switch.Switch)
 UartLogAction = uartlog_ns.class_("UartLogAction", cg.Component)
 
-# The main configuration schema for the UART log component.
+# The configuration schema for the UART log component.
 CONFIG_SCHEMA = cv.All(
     cv.Schema({
         cv.GenerateID(): cv.declare_id(UartLogComponent),
@@ -31,24 +33,29 @@ CONFIG_SCHEMA = cv.All(
     cv.COMPONENT_SCHEMA
 )
 
-def to_code(config):
-    var = cg.new_Pvariable(config[cv.GenerateID()], UartLogComponent)
-    yield cg.register_component(var, config)
+# Define the switch schema for our custom switch.
+UARTLOG_SWITCH_SCHEMA = switch.switch_schema(UartLogComponent).extend({
+    cv.Required(CONF_ID): cv.use_id(UartLogComponent),
+})
+
+# Define an async function to register the switch.
+async def uartlog_switch_to_code(config, key, template_args):
+    # Get the parent UART log component.
+    parent = await cg.get_variable(config[CONF_ID])
+    # Create a new PVariable for the switch.
+    var = cg.new_Pvariable(config[CONF_ID] + "_switch", parent)
+    cg.add(var.set_parent(parent))
+    return var
+
+# Main to_code function.
+async def to_code(config):
+    # Instantiate and register the main component.
+    var = cg.new_Pvariable(config[CONF_ID], UartLogComponent)
+    await cg.register_component(var, config)
     cg.add(var.set_enable_uart_log(config[CONF_ENABLE_UART_LOG]))
     cg.add(var.set_baud_rate(config[CONF_BAUD_RATE]))
     cg.add(var.set_tx_pin(config[CONF_TX_PIN]))
     cg.add(var.set_strip_colors(config[CONF_STRIP_COLORS]))
     cg.add(var.set_min_log_level(LOG_LEVELS[config[CONF_MIN_LEVEL]]))
-    yield var
-
-# Now, register the switch platform using the UART log component.
-# This schema makes sure that the switch uses the same component ID.
-UARTLOG_SWITCH_SCHEMA = switch.switch_schema(UartLogComponent).extend({
-    cv.Required(CONF_ID): cv.use_id(UartLogComponent),
-})
-
-@switch.register_switch("uartlog", UARTLOG_SWITCH_SCHEMA)
-def uartlog_switch_to_code(config, key, template_args):
-    # Get the component instance.
-    var = yield cg.get_variable(config[CONF_ID])
-    yield var
+    # Register our custom switch with platform "uartlog".
+    await switch.register_switch("uartlog", UARTLOG_SWITCH_SCHEMA, uartlog_switch_to_code)
