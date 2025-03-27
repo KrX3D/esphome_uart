78e20222b186a62e0cd8886c61b9ee7dbfb303e8
KrX3D <KrX3D@users.noreply.github.com>
2025-03-27 19:43:36 +0100

    ds

--- components/uartlog/uartlog_component.h
+++ components/uartlog/uartlog_component.h
@@ -17,7 +17,7 @@
 namespace esphome {
 namespace uartlog {
 
-class UartLogComponent : public Component {
+class UartLogComponent : public switch_::Switch, public Component {
  public:
   UartLogComponent() {}
 
