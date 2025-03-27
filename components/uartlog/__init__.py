4cdd54b2abc70fbaa31f709935c68d5b33ad56d7
KrX3D <KrX3D@users.noreply.github.com>
2025-03-28 00:15:31 +0100

    s

--- components/uartlog/__init__.py
+++ components/uartlog/__init__.py
@@ -2,9 +2,8 @@ import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.components import switch
 from esphome.components.logger import LOG_LEVELS, is_log_level
-from esphome.const import CONF_ID
 
-# Configuration keys for the UART log component.
+# Configuration keys for our UART log component.
 CONF_ENABLE_UART_LOG = "enable_uart_log"
 CONF_BAUD_RATE = "baud_rate"
 CONF_TX_PIN = "tx_pin"
@@ -15,14 +14,15 @@ CONF_MIN_LEVEL = "min_level"
 uartlog_ns = cg.esphome_ns.namespace("uartlog")
 
 # Define the main UART log component.
-# Note: We inherit from both cg.Component and switch.Switch so that our component can act as a switch.
+# Inherit from both cg.Component and switch.Switch so that our component can act as a switch.
 UartLogComponent = uartlog_ns.class_("UartLogComponent", cg.Component, switch.Switch)
 UartLogAction = uartlog_ns.class_("UartLogAction", cg.Component)
 
 # Main configuration schema.
+# All keys are wrapped in cv.Optional.
 CONFIG_SCHEMA = cv.All(
     cv.Schema({
-         cv.GenerateID(): cv.declare_id(UartLogComponent),
+         cv.Optional("id"): cv.declare_id(UartLogComponent),
          cv.Optional(CONF_ENABLE_UART_LOG, default=True): cv.boolean,
          cv.Optional(CONF_BAUD_RATE, default=115200): cv.positive_int,
          cv.Optional(CONF_TX_PIN, default=1): cv.int_range(min=0, max=40),
@@ -34,27 +34,28 @@ CONFIG_SCHEMA = cv.All(
 )
 
 # Define the switch schema.
-# (We do not include an "id" key here because cv.GenerateID() handles ID creation.)
+# Here we do not force an "id" key, so users may omit it.
 UARTLOG_SWITCH_SCHEMA = switch.switch_schema(UartLogComponent)
 
-# Async function to register our custom switch.
+# Async function to register the custom switch.
 async def uartlog_switch_to_code(config, key, template_args):
     # Get the parent UART log component.
-    parent = await cg.get_variable(config[CONF_ID])
-    # Create a new PVariable for the switch.
-    var = cg.new_Pvariable(config[CONF_ID] + "_switch", parent)
+    parent = await cg.get_variable(config.get("id"))
+    # Create a new PVariable for the switch (we append a suffix to create a unique ID).
+    var = cg.new_Pvariable(config.get("id", "") + "_switch", parent)
     cg.add(var.set_parent(parent))
     return var
 
 # Main async to_code function.
 async def to_code(config):
     # Instantiate and register the main UART log component.
-    var = cg.new_Pvariable(config[cv.GenerateID()], UartLogComponent)
+    # If the user provided an "id", use it; otherwise, auto-generate one.
+    var = cg.new_Pvariable(config.get("id"), UartLogComponent)
     await cg.register_component(var, config)
     cg.add(var.set_enable_uart_log(config[CONF_ENABLE_UART_LOG]))
     cg.add(var.set_baud_rate(config[CONF_BAUD_RATE]))
     cg.add(var.set_tx_pin(config[CONF_TX_PIN]))
     cg.add(var.set_strip_colors(config[CONF_STRIP_COLORS]))
     cg.add(var.set_min_log_level(LOG_LEVELS[config[CONF_MIN_LEVEL]]))
-    # Register the custom switch with platform "uartlog".
+    # Register the custom switch platform "uartlog".
     await switch.register_switch("uartlog", UARTLOG_SWITCH_SCHEMA, uartlog_switch_to_code)
