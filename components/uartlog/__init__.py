a282f4b15742f85fe49684a59748b832f00d7453
KrX3D <KrX3D@users.noreply.github.com>
2025-03-27 19:27:05 +0100

    ds

--- components/uartlog/__init__.py
+++ components/uartlog/__init__.py
@@ -1,44 +1,58 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
-from esphome.components.logger import LOG_LEVELS, is_log_level
-from esphome.const import CONF_ID
 from esphome import automation
+from esphome.components.logger import LOG_LEVELS, is_log_level
 
-# Configuration keys for our uartlog component.
-CONF_ENABLE_UART_LOG = "enable_uart_log"
+# These names are not defined in the core so we use literal strings.
 CONF_BAUD_RATE = "baud_rate"
 CONF_TX_PIN = "tx_pin"
+CONF_ENABLE_UART_LOG = "enable_uart_log"
 CONF_STRIP_COLORS = "strip_colors"
 CONF_MIN_LEVEL = "min_level"
-CONF_ALWAYS_FULL_LOGS = "always_full_logs"
+
+DEPENDENCIES = ['logger']
 
 uartlog_ns = cg.esphome_ns.namespace('uartlog')
 UartLogComponent = uartlog_ns.class_('UartLogComponent', cg.Component)
+UartLogAction = uartlog_ns.class_('UartLogAction', automation.Action)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(UartLogComponent),
-    cv.Optional(CONF_ENABLE_UART_LOG, default=True): cv.boolean,
-    cv.Optional(CONF_BAUD_RATE, default=115200): cv.positive_int,
-    cv.Optional(CONF_TX_PIN, default=1): cv.int_range(min=0, max=40),
-    cv.Optional(CONF_STRIP_COLORS, default=True): cv.boolean,
-    cv.Optional(CONF_MIN_LEVEL, default="DEBUG"): is_log_level,
-    cv.Optional(CONF_ALWAYS_FULL_LOGS, default=False): cv.boolean,
+CONFIG_SCHEMA = cv.All(
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(UartLogComponent),
+        cv.Optional(CONF_ENABLE_UART_LOG, default=True): cv.boolean,
+        cv.Optional(CONF_BAUD_RATE, default=115200): cv.positive_int,
+        # Replace cv.pin with an integer range validator
+        cv.Optional(CONF_TX_PIN, default=1): cv.int_range(min=0, max=40),
+        cv.Optional(CONF_STRIP_COLORS, default=True): cv.boolean,
+        cv.Optional(CONF_MIN_LEVEL, default="DEBUG"): is_log_level,
+    }),
+    cv.only_with_arduino,
+)
+
+UARTLOG_LOG_ACTION_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.use_id(UartLogComponent),
+    cv.Required("level"): cv.templatable(cv.int_range(min=0, max=7)),
+    cv.Required("tag"): cv.templatable(cv.string),
+    cv.Required("payload"): cv.templatable(cv.string),
 })
 
 def to_code(config):
     var = cg.new_Pvariable(config[cv.GenerateID()])
+    yield cg.register_component(var, config)
     cg.add(var.set_enable_uart_log(config[CONF_ENABLE_UART_LOG]))
     cg.add(var.set_baud_rate(config[CONF_BAUD_RATE]))
     cg.add(var.set_tx_pin(config[CONF_TX_PIN]))
     cg.add(var.set_strip_colors(config[CONF_STRIP_COLORS]))
     cg.add(var.set_min_log_level(LOG_LEVELS[config[CONF_MIN_LEVEL]]))
-    cg.add(var.set_always_full_logs(config[CONF_ALWAYS_FULL_LOGS]))
-    yield var
 
-#
-# Register a custom service to set (toggle) the UART logging at runtime.
-#
-UARTLOG_SERVICE_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.use_id(UartLogComponent),
-    cv.Required("enable"): cv.boolean,
-})
+@automation.register_action('uartlog.log', UartLogAction, UARTLOG_LOG_ACTION_SCHEMA)
+def uartlog_log_action_to_code(config, action_id, template_arg, args):
+    parent = yield cg.get_variable(config[cv.GenerateID()])
+    var = cg.new_Pvariable(action_id, template_arg, parent)
+    template_ = yield cg.templatable(config["level"], args, cg.uint8)
+    cg.add(var.set_level(template_))
+    template_ = yield cg.templatable(config["tag"], args, cg.std_string)
+    cg.add(var.set_tag(template_))
+    template_ = yield cg.templatable(config["payload"], args, cg.std_string)
+    cg.add(var.set_payload(template_))
+    yield var
--- components/uartlog/uartlog_component.h
+++ components/uartlog/uartlog_component.h
@@ -4,14 +4,13 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/defines.h"
-#ifdef USE_SWITCH
-#include "esphome/components/switch/switch.h"
-#endif
+#include "esphome/core/automation.h"
+#include "esphome/core/log.h"
 
 #ifdef ESP32
   #include "HardwareSerial.h"
 #elif defined(ESP8266)
-  #include <Arduino.h>
+  #include <Arduino.h>   // For Serial1 on ESP8266 (note: TX pin is fixed)
 #endif
 
 namespace esphome {
@@ -21,48 +20,50 @@ class UartLogComponent : public Component {
  public:
   UartLogComponent() {}
 
-  float get_setup_priority() const override { return setup_priority::AFTER_WIFI; }
+  // Component methods
   void setup() override;
   void loop() override;
-  void log(uint8_t level, const std::string &tag, const std::string &payload);
-  void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::AFTER_WIFI; }
 
-  // Setters to configure the component
-  void set_enable_uart_log(bool en) { this->enable_uart_log = en; }
+  // Setters for configuration options
   void set_baud_rate(uint32_t baud_rate) { this->baud_rate = baud_rate; }
   void set_tx_pin(uint8_t tx_pin) { this->tx_pin = tx_pin; }
-  void set_strip_colors(bool strip) { this->strip_colors = strip; }
+  void set_enable_uart_log(bool en) { this->enable_uart_log = en; }
   void set_min_log_level(int log_level) { this->min_log_level = log_level; }
-  void set_always_full_logs(bool always) { this->always_full_logs = always; }
+  void set_strip_colors(bool strip_colors) { this->strip_colors = strip_colors; }
+
+  // The log function that outputs via UART
+  void log(uint8_t level, const std::string &tag, const std::string &payload);
 
  protected:
   bool strip_colors{true};
   bool enable_uart_log{true};
-  bool always_full_logs{false};  // If true, bypass min_level filtering.
   uint32_t baud_rate{115200};
   uint8_t tx_pin{1};
-  int min_log_level{7}; // Used to filter out lower-priority logs.
+  int min_log_level{7}; // default, note: lower values mean higher priority
 #ifdef ESP32
   HardwareSerial *uart_{nullptr};
 #elif defined(ESP8266)
+  // On ESP8266, Serial1 is used (TX only, fixed pin)
   HardwareSerial *uart_{&Serial1};
 #endif
 };
 
-#ifdef USE_SWITCH
-class UartLogSwitch : public switch_::Switch, public Component {
+// An automation action to trigger a UART log manually.
+template<typename... Ts> class UartLogAction : public Action<Ts...> {
  public:
-  void set_parent(UartLogComponent *parent) { this->parent_ = parent; }
-  void write_state(bool state) override {
-    if (this->parent_ != nullptr) {
-      this->parent_->set_enable_uart_log(state);
-    }
-    publish_state(state);
+  UartLogAction(UartLogComponent *parent) : parent_(parent) {}
+  TEMPLATABLE_VALUE(uint8_t, level)
+  TEMPLATABLE_VALUE(std::string, tag)
+  TEMPLATABLE_VALUE(std::string, payload)
+
+  void play(Ts... x) override {
+    this->parent_->log(this->level_.value(x...), this->tag_.value(x...), this->payload_.value(x...));
   }
+
  protected:
-  UartLogComponent *parent_{nullptr};
+  UartLogComponent *parent_;
 };
-#endif
 
 }  // namespace uartlog
 }  // namespace esphome
